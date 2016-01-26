99a7a7389ba669406244045b9427b5bc01b58dac
kuman514 <7iggif@naver.com>
2016-01-27 08:48:33 +0900

    Priority Queue on C++, Priority on a lower number, template used.

--- 20160126PriorityQueue.cpp
+++ 20160126PriorityQueue.cpp
@@ -1,165 +1,118 @@
 #include <iostream>
-#define nullptr (Node*)NULL
+#define nullptr (Node<T>*)NULL
 
-unsigned int twoPower(unsigned int index)
-{
-	unsigned int result = 2;
-	while(result < index)
-		result *= 2;
-	return result;
-}
+// I think I misunderstood what Priority Queue means.
+// What Priority Queue means, I found, is a type of QUEUE, not a Binary Tree.
+// Later, my Heap will be added as soon as possible.
 
+template <typename T>
 struct Node
 {
-	Node *LChild;
-	int data;
-	unsigned int index;
-	Node *RChild;
-
-	Node *Parent;
+	Node<T> *prev;
+	T data;
+	Node<T> *next;
 };
 
+template <typename T>
 struct PQueue
 {
-//	private:
-		Node *root;
-		Node *cur;
-		unsigned int nowIndex;
-
-	public:
-		PQueue(int init = 0) : nowIndex(2)
-		{
-			root = new Node;
-			root -> data = init;
-			root -> index = 1;
-			
-			root -> LChild = nullptr;
-			root -> RChild = nullptr;
-			root -> Parent = root;
-
-			cur = root;
-			
-			/*
-			std::cout << root << ", ";
-			std::cout << "Index : " << root -> index << ", ";
-			std::cout << "Data : " << root -> data << std::endl;
-			*/
-		}
-		~PQueue(){}
+	private:
+		Node<T> *head;
+		Node<T> *tail;
 	
-		void enQueue(int num)
+	public:
+		PQueue()
 		{
-			bool keepGoing = 1;
-			int swap;
-			Node *tmp = new Node;
-			tmp -> data = num;
-			tmp -> index = nowIndex;
-			
-			tmp -> LChild = nullptr;
-			tmp -> RChild = nullptr;
+			head = new Node<T>;
+			tail = new Node<T>;
 
-			while(keepGoing)
+			head -> next = tail;
+			head -> prev = head;
+
+			tail -> next = tail;
+			tail -> prev = head;
+		}
+		~PQueue()
+		{
+			while(head -> next != tail)
+				dequeue();
+
+			delete head;
+			delete tail;
+		}
+		
+		void enqueue(T num)
+		{
+			Node<T> *search = head -> next;
+			Node<T> *insert = new Node<T>;
+			insert -> data = num;
+
+			while(search != tail)
 			{
-				if(cur -> LChild == nullptr)
-				{
-					cur -> LChild = tmp;
-					tmp -> Parent = cur;
-					keepGoing = 0;
-				}
-				else if(cur -> RChild == nullptr)
-				{
-					cur -> RChild = tmp;
-					tmp -> Parent = cur;
-					keepGoing = 0;
-				}
-				else
-				{
-					if(cur -> index == twoPower(cur -> index) - 1)
-					{
-						while(cur != root)
-							cur = cur -> Parent;
+				if(num < search -> data)
+					break;
 
-						while(cur -> LChild != nullptr && cur -> RChild != nullptr)
-							cur = cur -> LChild;
-					}
-					else if(cur == cur -> Parent -> LChild)
-						cur = cur -> Parent -> RChild;
-					else if(cur == cur -> Parent -> RChild)
-					{
-						while(cur != cur -> Parent -> LChild)
-							cur = cur -> Parent;
-
-						cur = cur -> Parent -> RChild;
-						
-						while(cur -> LChild != nullptr && cur -> RChild != nullptr)
-							cur = cur -> LChild;
-					}
-				}
+				search = search -> next;
 			}
 
-			/*
-			std::cout << tmp << ", ";
-			std::cout << "Index : " << tmp -> index << ", ";
-			std::cout << "Data : " << tmp -> data << ", ";
-			std::cout << "Parent : " << tmp -> Parent << std::endl;
-			*/
-			nowIndex++;
+			insert -> prev = search -> prev;
+			insert -> next = search;
 
-			// while the leaf tmp is bigger than their parents
-			while(tmp -> data < tmp -> Parent -> data)
+			search -> prev -> next = insert;
+			search -> prev = insert;
+		}
+		
+		T dequeue(void)
+		{
+			T pop = head -> next -> data;
+			Node<T> *remove = head -> next;
+
+			if(isEmpty())
 			{
-				// before swap
-				swap = tmp -> data;
-				tmp -> data = tmp -> Parent -> data;
-				tmp -> Parent -> data = swap;
-				tmp = tmp -> Parent;
+				std::cout << "Error : Empty Queue" << std::endl;
+				return -1;
+			}
 
-				// after swap
-				if(tmp -> LChild != nullptr && tmp -> data > tmp -> LChild -> data)
-				{
-					swap = tmp -> data;
-					tmp -> data = tmp -> LChild -> data;
-					tmp -> LChild -> data = swap;
-				}
-				else if(tmp -> RChild != nullptr && tmp -> data > tmp -> RChild -> data)
-				{
-					swap = tmp -> data;
-					tmp -> data = tmp -> RChild -> data;
-					tmp -> RChild -> data = swap;
-				}
+			head -> next = remove -> next;
+			remove -> next -> prev = head;
+
+			remove -> next = nullptr;
+			remove -> prev = nullptr;
+			remove -> data = (T)0;
+
+			delete remove;
+			return pop;
+		}
+
+		void show(void)
+		{
+			Node<T> *scan = head -> next;
+			int i = 1;
+
+			while(scan != tail)
+			{
+				std::cout << i++ << " : " << scan -> data << std::endl;
+				scan = scan -> next;
 			}
 		}
 
-		int deQueue(void)
-		{
-			int result = root -> data;
-
-			// blah blah
-
-			return result;
-		}
+		bool isEmpty(void) { return (head -> next == tail); }
 };
-
-void testprint(Node *pr)
-{
-	if(pr == nullptr)
-		return;
-
-	std::cout << pr << ", Index : " << pr -> index << ", Data : " << pr -> data << std::endl;
-
-	testprint(pr -> LChild);
-	testprint(pr -> RChild);
-}
 
 int main(void)
 {
-	PQueue pq(20);
+	PQueue<int> pq;
+	pq.enqueue(6);
+	pq.enqueue(3);
+	pq.enqueue(10);
+	pq.enqueue(1);
+	pq.enqueue(4);
+	pq.enqueue(7);
+	pq.show();
 
-	for(int i = 20; i >= 10; i--)
-	{
-		pq.enQueue(i);
-	}
-
-	testprint(pq.root);
+	std::cout << "DEQUEUE : " << pq.dequeue() << std::endl;
+	std::cout << "DEQUEUE : " << pq.dequeue() << std::endl;
+	
+	pq.show();
 	return 0;
 }
